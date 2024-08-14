7d5ed2c5c51d451be2d8748a3a52e1d834f78ca8
Karina Sodré <karinasodre.ts@gmail.com>
2024-08-14 16:57:40 -0300

    finalizado

---
+++ ContractSystem/src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+
+import entities.Contract;
+import entities.Installment;
+import services.ContractService;
+import services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre com os dados do contrato: ");
+        System.out.print("Número: ");
+        int number = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o número de parcelas: ");
+        int n = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+        contractService.processContract(obj, n);
+
+        System.out.println("Parcelas: ");
+        for (Installment installment: obj.getInstalments()) {
+            System.out.println(installment);
+        }
+
+        sc.close();
+    }
+}
